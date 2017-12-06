95a1c83160ed69b815979108ac6ecece7fb78606
David Brennan <davidjabrennan@gmail.com>
2017-12-06 07:39:21 +0000

    Day 6 part 1 finished

---
+++ day06/Day 6 Challenge 1.py
@@ -0,0 +1,21 @@
+inputFile = open("input.txt").read()
+
+example = "0 2 7 0"
+
+memory_banks = map(int, inputFile.split())
+
+redistributions = []
+
+while len(redistributions) == len(set(redistributions)):
+    biggest_bank = memory_banks.index(max(memory_banks))
+    blocks_in_bank = memory_banks[biggest_bank]
+    memory_banks[biggest_bank] = 0
+    current_bank = biggest_bank
+    while blocks_in_bank > 0:
+        next_bank_index = 0 if current_bank == len(memory_banks) - 1 else current_bank + 1
+        memory_banks[next_bank_index] += 1
+        blocks_in_bank -= 1
+        current_bank = next_bank_index
+    redistributions.append(str(memory_banks))
+
+print len(redistributions)
